9c97ef519f1e84344b2c4d0fc27b13c34df216b7
Agalindo25 <79726229+Agalindo25@users.noreply.github.com>
2021-02-26 16:19:20 -0600

    Primer Programa Hola Mundo

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string.h>
+#include <stdio.h>
+
+using std::cout;
+using std::cin;
+using std::string;
+using std::endl;
+
+int main()
+{
+    string nombre;
+    cout << "introduce tu nombre:" ;
+    cin >> nombre;
+    printf("\n");
+    cout << "hola " << nombre << endl;
+
+    return 0;
+
+}
